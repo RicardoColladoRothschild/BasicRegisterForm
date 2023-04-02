11523002dd47b4c2e9bcc8a64ade71c0eaaa35da
ricardocollado <ricardocolladorothschild@gmail.com>
2023-04-02 13:24:38 -0400

    hemos agregado por completo la funcionalidad del boton registar, ahora toma la informacion, y la compara con unos datos, dando un mensaje diferente para cada caso

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ let lastName;
 function getValueInput(){
     firstName = document.getElementById("domNombreText").value;
     lastName = document.getElementById("domApellidoText").value;
+        if(firstName=="ricardo"){
+            alert(`Bienvenido ${firstName +` `+ lastName}`);
+        }else{
+            alert("Usted no esta registrado");
+        }
+    
 }
